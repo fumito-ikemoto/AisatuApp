8c6657f741db449e7fb0b88d11e0b7895ae10755
fumito-ikemoto <fumito-ikemoto@united.jp>
2020-03-30 18:55:19 +0900

    時刻に応じたあいさつを行うアプリの実装

--- app/src/main/java/jp/techacademy/fumito/ikemoto/aisatuapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/fumito/ikemoto/aisatuapp/MainActivity.kt
@@ -1,12 +1,43 @@
 package jp.techacademy.fumito.ikemoto.aisatuapp
 
+import android.app.TimePickerDialog
+import android.util.Log
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dialogButton.setOnClickListener()
+        {
+            openTimePickerDialog()
+        }
     }
+
+    private fun openTimePickerDialog()
+    {
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { _, hour, _ ->
+                var greetingText = generateGreetingText(hour)
+                timeText.text = greetingText
+            },
+            12, 0, true)
+        timePickerDialog.show()
+    }
+
+    private fun generateGreetingText(hour : Int) : String
+    {
+        when(hour)
+        {
+            in 2 until 10 -> return "おはよう"
+            in 10 until 18 -> return "こんにちは"
+            else -> return "こんばんは"
+        }
+    }
+
 }
